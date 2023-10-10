4d5003366238f414544feb2dd8142ed8ded33b21
Patrik Ferreira <patrik.andradef@gmail.com>
2023-10-09 22:24:32 -0300

    first commit

---
+++ src/main/java/com/example/todolist/user/UserController.java
@@ -0,0 +1,15 @@
+package com.example.todolist.user;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    
+
+    public void crate(@RequestBody UserModel userModel) {
+
+    }
+}
---
+++ src/main/java/com/example/todolist/user/UserModel.java
@@ -0,0 +1,7 @@
+package com.example.todolist.user;
+
+public class UserModel {
+    public String username;
+    public String name;
+    public String password;
+}
